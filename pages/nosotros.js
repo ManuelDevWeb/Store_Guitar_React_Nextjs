440be7ef53f1b5b88a9482dafa45ce16e39e4cd0
ManuelDev <manueldeveloper17@gmail.com>
2022-11-11 23:45:17 -0500

    Modify nosotros

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -10,7 +10,7 @@ const Nosotros = () => {
       description={"Sobre Nosotros, GuitarLA, tienda de musica!"}
     >
       <h1>Desde Nosotros</h1>
-      <Link href="/">Ir a Inicio</Link>
+      <Link href="/">Ir a Inicio!</Link>
     </Layout>
   );
 };
