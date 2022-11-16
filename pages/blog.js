16363084082b2707a50e4f83e8898d34325e7149
ManuelDev <manueldeveloper17@gmail.com>
2022-11-16 08:54:54 -0500

    Add component post and helper functions

--- pages/blog.js
+++ pages/blog.js
@@ -1,13 +1,39 @@
 // Components
 import { Layout } from "../components/layout";
+import { Post } from "../components/post";
 
-const Blog = () => {
+// Styles
+import styles from "../styles/grid.module.css";
+
+// Consultando API
+
+// Se recomienda StaticProps cuando la información no cambia en cada request: Una página informativa, documentación, trámites, anuncios.
+export async function getStaticProps() {
+  const res = await fetch(`${process.env.API_URL}/posts/?populate=imagen`);
+
+  const { data: posts } = await res.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+const Blog = ({ posts }) => {
   return (
     <Layout
       title={"Blog"}
       description={"Sobre Nosotros, GuitarLA, tienda de musica!"}
     >
-      <h1>Blog</h1>
+      <main className="contenedor">
+        <h2 className="heading">Blog</h2>
+        <div className={styles.grid}>
+          {posts?.map((post) => (
+            <Post key={post.id} post={post.attributes} />
+          ))}
+        </div>
+      </main>
     </Layout>
   );
 };
