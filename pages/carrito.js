b5012869b912c0ffe0cef3c88f23fd3d22a7b57d
ManuelDev <manueldeveloper17@gmail.com>
2022-11-25 12:18:04 -0500

    Add button to select count guitars

--- pages/carrito.js
+++ pages/carrito.js
@@ -10,12 +10,12 @@ const Carrito = () => {
       <main className="contenedor">
         <h1 className="heading">Carrito</h1>
 
-        <div>
-          <div>
+        <div className={styles.contenido}>
+          <div className={styles.carrito}>
             <h2>Articulos</h2>
           </div>
 
-          <aside>
+          <aside className={styles.resumen}>
             <h3>Resumen del Pedido</h3>
             <p>Total a pagar: </p>
           </aside>
